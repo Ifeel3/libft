34c2f6da174308d2267c542269d07528ae9e3b06
lvallie <lvallie@student.21-school.ru>
2021-05-10 16:09:09 +0300

    fixed ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lvallie <lvallie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 23:42:27 by lvallie           #+#    #+#             */
-/*   Updated: 2021/05/09 05:09:48 by lvallie          ###   ########.fr       */
+/*   Updated: 2021/05/10 16:06:17 by lvallie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,25 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*new;
 	char	*tmp;
 	char	*src;
-	size_t	strlen;
 
+	if (!s)
+		return (NULL);
 	src = (char *)s + start;
-	strlen = ft_strlen(s);
+	if (start > ft_strlen(s))
+	{
+		new = malloc(sizeof(char) * 1);
+		new[0] = 0;
+		return (new);
+	}
 	if (ft_strlen(src) >= len)
 		new = malloc(sizeof(char) * (len + 1));
 	else
 		new = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (!new)
+		return (NULL);
 	tmp = new;
-	while (len && *src)
-	{
+	while (len-- && *src)
 		*tmp++ = *src++;
-		len--;
-	}
 	*tmp = 0;
 	return (new);
 }
